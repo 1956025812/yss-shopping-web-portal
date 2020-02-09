c17670f4e73bde71c127cfebd39b048dce3ffa17
1956025812 <1956022812@qq.com>
2020-02-09 18:05:02 +0800

    左侧菜单树接口调通OK

---
+++ yss-shopping-web-portal/src/api/userManager/menu.js
@@ -0,0 +1,14 @@
+import axios from '@/libs/api.request'
+
+
+/**
+ * @description 查询菜单列表
+ */
+export const selectSysMenuListAPI = (params) => {
+    return axios.request({
+        url: '/sysMenu/list',
+        method: 'get',
+        params: params
+    });
+}
+
--- yss-shopping-web-portal/src/view/pages/userManager/menu/menuTreePage.vue
+++ yss-shopping-web-portal/src/view/pages/userManager/menu/menuTreePage.vue
@@ -1,20 +1,52 @@
 <template>
-  <div>页面菜单树列表</div>
+  <Tree :data="menuTreeData"></Tree>
 </template>
 <script>
 import { getToken } from "@/libs/util.js";
+import { selectSysMenuListAPI } from "@/api/userManager/menu.js";
 
 export default {
   name: "MenuTreePageComponent",
   data() {
-    return {};
+    return {
+      menuTreeData: []
+    };
   },
 
   methods: {
     /**
      * 初始化系统树数据
      */
-    initMenuTreeData() {}
+    initMenuTreeData() {
+      this.menuTreeData = [];
+
+      let params = new Object();
+      params.type = 1;
+      selectSysMenuListAPI(params).then(res => {
+        if (res.data.code == 1) {
+          let sysMenuOutVOList = res.data.data;
+          if (null != sysMenuOutVOList && sysMenuOutVOList.length > 0) {
+            sysMenuOutVOList.forEach(eachSysMenuOutVO => {
+              let menuTreeNode = new Object();
+              menuTreeNode.loading = false;
+              menuTreeNode.expand = false;
+              menuTreeNode.mid = eachSysMenuOutVO.mid;
+              menuTreeNode.level = eachSysMenuOutVO.level;
+              menuTreeNode.menuCode = eachSysMenuOutVO.menuCode;
+              menuTreeNode.title = eachSysMenuOutVO.menuName;
+              menuTreeNode.parentId = eachSysMenuOutVO.parentId;
+
+              // TODO 怎么按层级结构拼接
+              this.menuTreeData.push(menuTreeNode);
+            });
+          }
+        } else if (res.data.code == 0) {
+          this.$Notice.error({
+            desc: res.data.msg
+          });
+        }
+      });
+    }
   },
 
   created() {
